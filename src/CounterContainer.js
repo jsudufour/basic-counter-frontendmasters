206e40e9c5500280b7fad670ca6b25ed1870a9fc
Josiane Dufour <josiane@Josianes-MacBook-Pro.local>
2019-05-11 13:24:12 +0100

    refactor basic counter to separate presentational and state components, by putting state component into container component and keeping presentational component separate as child component to the container one

--- src/CounterContainer.js
+++ src/CounterContainer.js
@@ -1,1 +1,45 @@
-// Intentionally left blank.
+import React, { Component } from "react";
+
+import Counter from "./Counter";
+
+export default class CounterContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+    // this.handleIncrement = this.handleImcrement.bind(this)
+  }
+
+  // actually defines the function on the instance
+  // arrow function binds it to the instance
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  handleDecrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      count: 0
+    });
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <Counter
+        count={count}
+        onIncrement={this.handleIncrement}
+        onDecrement={this.handleDecrement}
+        onReset={this.reset}
+      />
+    );
+  }
+}
